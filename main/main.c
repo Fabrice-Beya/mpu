f74e8399d37632e5e303d4acee322b3bc9ac1f5e
Fabrice Beya <byxfab001@myuct.ac.za>
2022-10-09 13:28:10 +0200

    Update main.c

--- main/main.c
+++ main/main.c
@@ -1,3 +1,18 @@
+
+
+/*****************************************************************************
+* | File      	:   main.c
+* | Author      :   Fabrice Beya
+* | Function    :   Robot car application
+* | Info        :
+*----------------
+* |	This version:   V1.0
+* | Date        :   2022-09-18
+* | Info        :   
+#
+# Summary
+******************************************************************************/
+
 #include "main.h"
 
 void *Thread_Main_Controller(void *vargp) {
